afea3158abdd973f0781619c1ad1ee0ed9ab1837
CristianMiron0 <cristian.miron98@gmail.com>
2023-05-15 10:23:23 +0200

    Forma final

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using static System.Console;
 
 class Program
 {
@@ -6,55 +6,55 @@ class Program
     {
         while (true)
         {
-            Console.Clear();
-            Console.Write("Ingrese el nombre del deporte: (Fútbol/Baloncesto/Béisbol)");
-            string nombre = Console.ReadLine()?.Trim();
+            Clear();
+            Write("Ingrese el nombre del deporte: (Fútbol/Baloncesto/Béisbol)");
+            string? nombre = ReadLine()?.Trim();
 
-            Console.Write("Ingrese el número de jugadores: ");
+            Write("Ingrese el número de jugadores: ");
             int jugadores;  
-            while (!int.TryParse(Console.ReadLine(), out jugadores) || jugadores <= 0)
+            while (!int.TryParse(ReadLine(), out jugadores) || jugadores <= 0)
             {
-                Console.WriteLine("Ingrese un número de jugadores válido.");
+                WriteLine("Ingrese un número de jugadores válido.");
             }
 
             Deportes deporte;
 
-            switch (nombre.ToLowerInvariant())
+            switch (nombre!.ToLower())
             {
                 case "fútbol":
-                    Console.Write("Ingrese la posición del arquero: ");
-                    string posicionPortero = Console.ReadLine()?.Trim();
-                    deporte = new Futbol(nombre, jugadores, posicionPortero);
+                    Write("Ingrese la posición del arquero: ");
+                    string? posicionPortero = ReadLine()?.Trim();
+                    deporte = new Futbol(nombre, jugadores, posicionPortero!);
                     break;
 
                 case "baloncesto":
-                    Console.Write("Ingrese la altura del aro: ");
+                    Write("Ingrese la altura del aro: ");
                     double alturaAro;
-                    while (!double.TryParse(Console.ReadLine(), out alturaAro) || alturaAro <= 0)
+                    while (!double.TryParse(ReadLine(), out alturaAro) || alturaAro <= 0)
                     {
-                        Console.WriteLine("Ingrese una altura de aro válida.");
+                        WriteLine("Ingrese una altura de aro válida.");
                     }
                     deporte = new Baloncesto(nombre, jugadores, alturaAro);
                     break;
 
                 case "béisbol":
-                    Console.Write("Ingrese la descripción de la entrada extra: ");
+                    Write("Ingrese la descripción de la entrada extra: ");
                     double entradaExtra;
-                    while (!double.TryParse(Console.ReadLine(), out entradaExtra))
+                    while (!double.TryParse(ReadLine(), out entradaExtra))
                     {
-                        Console.WriteLine("Ingrese una descripción válida para la entrada extra.");
+                        WriteLine("Ingrese una descripción válida para la entrada extra.");
                     }
                     deporte = new Beisbol(nombre, jugadores, entradaExtra);
                     break;
 
                 default:
-                    Console.WriteLine("El deporte ingresado no es válido.");
+                    WriteLine("El deporte ingresado no es válido.");
                     continue;
             }
 
-            Console.WriteLine(deporte.Descripcion());
-            Console.Write("Entre cualquier tecla para continuar...");
-            Console.ReadKey(true);
+            WriteLine(deporte.Descripcion());
+            Write("Entre cualquier tecla para continuar...");
+            ReadKey(true);
         }
     }
 }
--- Readme.md
+++ Readme.md
@@ -3,7 +3,7 @@
 # Instrucciones de instalacion
 
 - Clonar el repositorio
-  - `git clone url https://github.com/CristianMiron0/FinalCSharp.git`
+  - `git clone url https://github.com/CristianMiron0/FirstAssesmentCSharp.git`
 
 - .NET Version: net7.0
 
@@ -13,53 +13,3 @@
     - ingrese el espacio donde se almacena el programa con `cd -ruta a la carpeta clonada-`
     - `dotnet build`
     - `dotnet run`
-
-
-## Este programa se crea con las siguientes instrucciones:
-
-### Realizar:
-
-## First assesment sprint EOI Cloud computing!
-
-#### Repositorio:
-
-- [x] FirstAssesmentCSharp
-
-### Introducción
-
-En este examen evaluaremos lo aprendido y puesto en práctica durante esta primera parte del curso de Cloud Computing. Y la aplicación de estos conceptos en el lenguaje CSharp con aplicaciones de consola.
-
-Descargará un escenario con base en el número de su código de sorteo.
-
-La duración del examen es de 60 minutos.
-
-### Instrucciones
-
-Descargar su examen con base en el código de sorteo. 
-
-En esta descarga encontrará un **escenario** que deberá resolver para completar con éxito el examen. Dicho **escenario** estará compuesto de las instrucciones que debe resolver y código que deberá usar para juntar con el suyo y completar el examen.
-
-#### Criterios de evaluación
-
-- [x] Creación de la carpeta de trabajo.
-
-- [x] Creación de un fichero aparte por cada programa de la solución, para el código que usted deba desarrollar durante el examen.
-- [x] Dividir en apropiados ficheros los archivos que forman parte de la solución del código que se presenta en el escenario.
-- [x] Instalar las librerías necesarias para la ejecución del programa si las necesitase. 
-- [x] La conexión de todos los elementos del programa en un programa principal.
-- [x] La prueba de datos con la información dada. Comprobación que el programa funciona interconectando todos sus elementos.
-
-- [x] La creación del fichero .gitignore que funcione e ignore lo que tiene que ignorar.
-
-- [x] La creación del repositorio local.
-
-- [x] El subir en el repositorio en GitHub indicado en su cuenta de GitHub en modo público. No deberá tener colaboradores.
-- [x] Readme.md con las instrucciones para descargar el proyecto y ejecutarlo y observaciones, para el caso de que existan bugs y no haya alcanzado el tiempo para resolverlos.
-
-> **Recuerde** que para iniciar un programa desde cero, debe utilizar la técnica de algoritmación que lo guiará por el camino de la solución.
->
-> a) Encuentre los datos de entrada
->
-> b) identifique el proceso, que debe ser realizado para dar con la solución, esto debe ser expresado en oraciones. Si es necesario consultarlo e investigarlo hagalo. Identifique repeticiones, decisiones y variables para llevar a cabo el proceso. Realice el código en el lenguaje apropiado con la sintaxis mas apropiada.
->
-> c) Identificar la salida, recuerde que un print o un WriteLine, no son los mejores para ponerlos en la salida, pues limitan el uso del programa a únicamente aplicaciones de consola.
